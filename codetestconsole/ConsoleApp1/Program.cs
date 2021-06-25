11b4cc8a9cd48d90427f000cd4dc3ea875d5903b
sirichindain <sirichindain.1310@gmail.com>
2021-06-26 01:42:00 +0530

    promo for A and B

--- codetestconsole/ConsoleApp1/Program.cs
+++ codetestconsole/ConsoleApp1/Program.cs
@@ -16,13 +16,33 @@ namespace ConsoleApp1
             List<OrderData> orderDetails = orders.TakeOrder();
             List<PromotionData> promotionList = Promotion.ActivePromotions();
 
-            foreach (PromotionData promo in promotionList)
+            int costOfPurchase = 0;
+            //foreach(PromotionData promo in promotionList)
+            //{
+            //    orderDetails.Where(o => o.ProductDetails.ProductName)
+            //}
+            foreach (OrderData ord in orderDetails)
             {
-                foreach(OrderData ord in orderDetails)
-                {
+                PromotionData promo = promotionList.Where(p => p.PromoProduct.ContainsKey(ord.ProductDetails.ProductName)).FirstOrDefault();
+
+                if(promo.PromoProduct.Count > 1)
+                { 
 
                 }
+                else
+                {
+                    int qunatity = promo.PromoProduct[ord.ProductDetails.ProductName];
+                    if (ord.Qunatity > qunatity)
+                    {
+                        costOfPurchase += Convert.ToInt32(ord.Qunatity / qunatity) * promo.PromotionPrice + (ord.Qunatity % qunatity) * ord.ProductDetails.Price;
+                    }
+                    else
+                    {
+                        costOfPurchase += ord.ProductDetails.Price * qunatity;
+                    }
+                }
             }
+            
                 
             
         }
--- codetestconsole/ConsoleApp1/Promotion.cs
+++ codetestconsole/ConsoleApp1/Promotion.cs
@@ -11,9 +11,9 @@ namespace ConsoleApp1
         public static List<PromotionData> ActivePromotions()
         {
             List<PromotionData> promotionDetails = new List<PromotionData>();
-            PromotionData promotionA = new PromotionData(1, new List<OrderData>() { new OrderData(new ProductData("A", 50), 3) }, 130);
-            PromotionData promotionB = new PromotionData(2, new List<OrderData>() { new OrderData(new ProductData("B", 30), 2) }, 45);
-            PromotionData promotionCD = new PromotionData(3, new List<OrderData>() { new OrderData(new ProductData("C", 20), 1), new OrderData(new ProductData("D", 15), 1) }, 30);
+            PromotionData promotionA = new PromotionData(1, new Dictionary<string, int>() { {"A",3 } }, 130);
+            PromotionData promotionB = new PromotionData(2, new Dictionary<string, int>() { { "B", 2 } }, 45);
+            PromotionData promotionCD = new PromotionData(3, new Dictionary<string, int>() { { "C", 1 }, { "D", 1 } }, 30);
             promotionDetails.AddRange(new PromotionData[] { promotionA, promotionB, promotionCD });
 
             return promotionDetails;
@@ -23,13 +23,13 @@ namespace ConsoleApp1
     public class PromotionData
     {
         public int PromotionId { get; set; }
-        public List<OrderData> OrderDetails { get; set; }
+        public Dictionary<string, int> PromoProduct { get; set; }
         public int PromotionPrice { get; set; }
 
-        public PromotionData(int promotionId, List<OrderData> orderDetails, int PromotionPrice)
+        public PromotionData(int promotionId, Dictionary<string,int> promoProduct, int PromotionPrice)
         {
             this.PromotionId = promotionId;
-            this.OrderDetails = orderDetails;
+            this.PromoProduct = promoProduct;
             this.PromotionPrice = PromotionPrice;
         }
     }
